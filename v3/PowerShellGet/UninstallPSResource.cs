baee66143f45cfa3ce5fc6f70faca850cfabfeaf
Amber Erickson <americks@microsoft.com>
2020-02-05 14:44:40 -0800

    Preliminary Uninstall implementation

---
+++ v3/PowerShellGet/UninstallPSResource.cs
@@ -0,0 +1,384 @@
+﻿
+using System;
+using System.Collections;
+using System.Management.Automation;
+using System.Collections.Generic;
+using NuGet.Configuration;
+using NuGet.Common;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using System.Threading;
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
+using System.Net;
+using System.Linq;
+using MoreLinq.Extensions;
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
+{
+
+
+    /// <summary>
+    /// Uninstall 
+    /// </summary>
+
+    [Cmdlet(VerbsLifecycle.Uninstall, "PSResource", DefaultParameterSetName = "NameParameterSet", SupportsShouldProcess = true,
+    HelpUri = "<add>", RemotingCapability = RemotingCapability.None)]
+    public sealed
+    class UninstallPSResource : PSCmdlet
+    {
+        //  private string PSGalleryRepoName = "PSGallery";
+
+        /// <summary>
+        /// Specifies the exact names of resources to install from a repository.
+        /// A comma-separated list of module names is accepted. The resource name must match the resource name in the repository.
+        /// </summary>
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, ParameterSetName = "NameParameterSet")]
+        [ValidateNotNullOrEmpty]
+        public string[] Name
+        {
+            get
+            { return _name; }
+
+            set
+            { _name = value; }
+        }
+        private string[] _name; // = new string[0];
+
+        /// <summary>
+        /// Used for pipeline input.
+        /// </summary>
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, ParameterSetName = "InputObjectSet")]
+        [ValidateNotNullOrEmpty]
+        public PSCustomObject[] InputObject
+        {
+            get
+            { return _inputObject; }
+
+            set
+            { _inputObject = value; }
+        }
+        private PSCustomObject[] _inputObject; // = new string[0];
+
+
+        /// <summary>
+        /// The destination where the resource is to be installed. Works for all resource types.
+        /// </summary>
+        [Parameter(ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, ParameterSetName = "NameParameterSet")]
+        [ValidateNotNullOrEmpty]
+        public string[] DestinationPath
+        {
+            get
+            { return _type; }
+
+            set
+            { _type = value; }
+        }
+        private string[] _type;
+
+        /// <summary>
+        /// Specifies the version or version range of the package to be installed
+        /// </summary>
+        [Parameter(ParameterSetName = "NameParameterSet")]
+        [ValidateNotNullOrEmpty]
+        public string Verison
+        {
+            get
+            { return _version; }
+
+            set
+            { _version = value; }
+        }
+        private string _version;
+
+        /// <summary>
+        /// Specifies to allow installation of prerelease versions
+        /// </summary>
+        [Parameter(ParameterSetName = "NameParameterSet")]
+        public SwitchParameter Prerelease
+        {
+            get
+            { return _prerelease; }
+
+            set
+            { _prerelease = value; }
+        }
+        private SwitchParameter _prerelease;
+
+        /// <summary>
+        /// Specifies a user account that has rights to find a resource from a specific repository.
+        /// </summary>
+        [Parameter(ParameterSetName = "NameParameterSet")]
+        [ValidateNotNullOrEmpty]
+        public string[] Repository
+        {
+            get
+            { return _repository; }
+
+            set
+            { _repository = value; }
+        }
+        private string[] _repository;
+
+        /// <summary>
+        /// Specifies the type of the resource to be searched for. 
+        /// </summary>
+        [Parameter(ValueFromPipeline = true, ParameterSetName = "NameParameterSet")]
+        [ValidateNotNull]
+        public string[] Tags
+        {
+            get
+            { return _tags; }
+
+            set
+            { _tags = value; }
+        }
+        private string[] _tags;
+
+        /// <summary>
+        /// Specify which repositories to search in.
+        /// </summary>
+        [ValidateNotNullOrEmpty]
+        [Parameter(ValueFromPipeline = true, ParameterSetName = "NameParameterSet")]
+        public string[] Repositories
+        {
+            get { return _repositories; }
+
+            set { _repositories = value; }
+        }
+        private string[] _repositories;
+
+        /// <summary>
+        /// Specifies a user account that has rights to find a resource from a specific repository.
+        /// </summary>
+        [Parameter(ValueFromPipelineByPropertyName = true, ParameterSetName = "NameParameterSet")]
+        public PSCredential Credential
+        {
+            get
+            { return _credential; }
+
+            set
+            { _credential = value; }
+        }
+        private PSCredential _credential;
+
+        /// <summary>
+        /// Specifies to return any dependency packages.
+        /// Currently only used when name param is specified.
+        /// </summary>
+        [Parameter()]
+        [ValidateSet("CurrentUser", "AllUsers")]
+        public string Scope
+        {
+            get { return _scope; }
+
+            set { _scope = value; }
+        }
+        private string _scope;
+
+
+        /// <summary>
+        /// Overrides warning messages about installation conflicts about existing commands on a computer.
+        /// Overwrites existing commands that have the same name as commands being installed by a module. AllowClobber and Force can be used together in an Install-Module command.
+        /// Prevents installing modules that have the same cmdlets as a differently named module already
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter NoClobber
+        {
+            get { return _noClobber; }
+
+            set { _noClobber = value; }
+        }
+        private SwitchParameter _noClobber;
+
+
+        /// <summary>
+        /// Suppresses being prompted if the publisher of the resource is different from the currently installed version.
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter IgnoreDifferentPublisher
+        {
+            get { return _ignoreDifferentPublisher; }
+
+            set { _ignoreDifferentPublisher = value; }
+        }
+        private SwitchParameter _ignoreDifferentPublisher;
+
+
+        /// <summary>
+        /// Suppresses being prompted for untrusted sources.
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter TrustRepository
+        {
+            get { return _trustRepository; }
+
+            set { _trustRepository = value; }
+        }
+        private SwitchParameter _trustRepository;
+
+        /// <summary>
+        /// Overrides warning messages about resource installation conflicts.
+        /// If a resource with the same name already exists on the computer, Force allows for multiple versions to be installed.
+        /// If there is an existing resource with the same name and version, Force does NOT overwrite that version.
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter Force
+        {
+            get { return _force; }
+
+            set { _force = value; }
+        }
+        private SwitchParameter _force;
+
+
+        /// <summary>
+        /// Overwrites a previously installed resource with the same name and version.
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter Reinstall
+        {
+            get { return _reinstall; }
+
+            set { _reinstall = value; }
+        }
+        private SwitchParameter _reinstall;
+
+
+        /// <summary>
+        /// Suppresses progress information.
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter Quiet
+        {
+            get { return _quiet; }
+
+            set { _quiet = value; }
+        }
+        private SwitchParameter _quiet;
+
+
+        /// <summary>
+        /// For modules that require a license, AcceptLicense automatically accepts the license agreement during installation.
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter AcceptLicense
+        {
+            get { return _acceptLicense; }
+
+            set { _acceptLicense = value; }
+        }
+        private SwitchParameter _acceptLicense;
+
+
+        /// <summary>
+        /// Returns the resource as an object to the console.
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter PassThru
+        {
+            get { return _passThru; }
+
+            set { _passThru = value; }
+        }
+        private SwitchParameter _passThru;
+
+
+        // This will be a list of all the repository caches
+        public static readonly List<string> RepoCacheFileName = new List<string>();
+        // Temporarily store cache in this path for testing purposes
+        public static readonly string RepositoryCacheDir = "c:/code/temp/repositorycache"; //@"%APPDTA%\NuGet";
+        private readonly object p;
+
+        // Define the cancellation token.
+        CancellationTokenSource source;
+        CancellationToken cancellationToken;
+
+
+        /// <summary>
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            source = new CancellationTokenSource();
+            cancellationToken = source.Token;
+
+
+            foreach (var pkgName in _name)
+            {
+                UninstallHelper(pkgName, cancellationToken);
+            }
+
+        }
+
+
+
+        /// just unintall module, not dependencies
+
+
+        private void UninstallHelper(string pkgName, CancellationToken cancellationToken)
+        {
+            // scope, admin rightsget
+
+
+            // GET INSTALLED PKG
+
+
+
+            /*
+            if (pkg == null)
+            {
+                throw new ArgumentNullException(paramName: "pkg");
+            }
+            */
+
+            var dirName = Path.Combine("C:/code/temp/installtestpath", pkgName);
+
+
+            // check versions
+
+            var dirNameVersion = Path.Combine(dirName, );
+
+            if (String.IsNullOrWhiteSpace(dirNameVersion) || !Directory.Exists(dirNameVersion))
+            {
+                return;
+            }
+
+
+
+            if (Directory.GetDirectories(dirName).Length > 1)
+            {
+                // If there's more than one version in the pkg Name directory, just delete the specific version
+                Directory.Delete(dirNameVersion, true);
+            }
+            else
+            {
+                // Otherwise delete 
+                Directory.Delete(dirName, true);
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
