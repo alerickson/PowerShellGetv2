796bc1ea112159d62fc9822483ba2f3fdd53131c
Amber Erickson <americks@microsoft.com>
2020-02-06 23:05:57 -0800

    Implement get-psresource

--- v3/PowerShellGet/GetPSResource.cs
+++ v3/PowerShellGet/GetPSResource.cs
@@ -7,6 +7,14 @@
 //using Microsoft.PowerShell.PowerShellGet.RepositorySettings;
 
 
+using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+
+
 
 namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 {
@@ -52,6 +60,20 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
         private string _version;
 
 
+        /// <summary>
+        /// Specifies to include prerelease versions
+        /// </summary>
+        [Parameter(ParameterSetName = "NameParameterSet")]
+        public SwitchParameter Prerelease
+        {
+            get
+            { return _prerelease; }
+
+            set
+            { _prerelease = value; }
+        }
+        private SwitchParameter _prerelease;
+
 
 
         /// <summary>
@@ -59,7 +81,87 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
         protected override void ProcessRecord()
         {
             // list all installation locations:
-            var root = "c:/code/temp/test";
+            var psModulePath = "C:\\code\\temp\\sinstalltestpath";
+
+
+
+            // 1) Create a list of either
+            // Of all names
+            var versionDirs = Directory.GetDirectories(psModulePath).ToList();
+
+            // Or a list of the passed in names
+            if (_name != null)
+            {
+                versionDirs = versionDirs.FindAll(p => _name.Contains(p));
+            }
+
+
+
+            NuGetVersion nugetVersion;
+            var bleh = NuGetVersion.TryParse(_version, out nugetVersion);
+
+            VersionRange versionRange = null;
+            if (nugetVersion == null)
+            {
+                VersionRange.TryParse(_version, out versionRange);
+            }
+
+
+
+            List<string> dirsToReturn = new List<string>();
+
+            // check whether we want to include prerelease versions or not
+            // If prereleaseOnly is not specified, we'll only take into account stable versions of pkgs
+            if (!_prerelease)
+            {
+                List<string> stableVersionDirs = new List<string>();
+                foreach (var dir in versionDirs)
+                {
+                    var nameOfDir = Path.GetFileName(dir);
+                    var nugVersion = NuGetVersion.Parse(nameOfDir);
+
+                    if (nugVersion.IsPrerelease)
+                    {
+                        stableVersionDirs.Remove(dir);
+                    }
+                }
+                versionDirs = stableVersionDirs;
+            }
+
+
+
+
+            //2) use above list to check 
+            // if the version specificed is a version range
+            if (versionRange != null)
+            {
+
+                foreach (var versionDirPath in versionDirs)
+                {
+                    var nameOfDir = Path.GetFileName(versionDirPath);
+                    var nugVersion = NuGetVersion.Parse(nameOfDir);
+
+                    if (versionRange.Satisfies(nugVersion))
+                    {
+                        dirsToReturn.Add(versionDirPath);
+                    }
+                }
+            }
+            else if (nugetVersion != null)
+            {
+                // if the version specified is a version
+
+                dirsToReturn.Add(nugetVersion.ToNormalizedString());
+            }
+            else
+            {
+                // if no version specified (just the latest version)
+                // if no version is specified, just get the latest version
+                versionDirs.Sort();
+
+                dirsToReturn.Add(versionDirs.First());
+            }
+
 
 
             /// list all the modules, or the specific name / version
@@ -81,5 +183,8 @@ namespace Microsoft.PowerShell.PowerShellGet.Cmdlets
 
     */
         }
+
+
+
     }
 }
